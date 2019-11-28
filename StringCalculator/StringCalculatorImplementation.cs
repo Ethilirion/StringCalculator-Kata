5dc12b134b350a468b015958ad19318e1def1511
Pierre Hamelin <pierre.hamelin@epitech.eu>
2019-11-28 19:01:43 +0100

    protection contre trop de séparateurs (loi 3)

--- StringCalculator/StringCalculatorImplementation.cs
+++ StringCalculator/StringCalculatorImplementation.cs
@@ -10,10 +10,18 @@ namespace Kata
         {
             if (InputStringIsEmpty(inputString))
                 return 0;
-
             IEnumerable<char> separators = InitializeSeparators(inputString);
+            if (TooManySeparators(inputString, separators))
+                throw new Exception("too many separators");
             IEnumerable<int> numbers = GetNumbersFromInput(inputString, separators);
             return ComputeNumbers(numbers);
+        }
+
+        private bool TooManySeparators(string inputString, IEnumerable<char> separators)
+        {
+            int numberOfSeparators = inputString.Count(separator => separators.Contains(separator));
+            int numberOfNumbers = GetNumbersFromInput(inputString, separators).Count();
+            return numberOfSeparators >= numberOfNumbers;
         }
 
         private int ComputeNumbers(IEnumerable<int> numbers)
@@ -25,6 +33,7 @@ namespace Kata
         {
             return from numberAsString
                    in inputString.Split(separators.ToArray())
+                   where numberAsString != string.Empty
                    select int.Parse(numberAsString);
         }
 
