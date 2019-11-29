701760ca05fd9d123cab37897ae3d564aeb61bc5
Pierre Hamelin <pierre.hamelin@epitech.eu>
2019-11-29 11:48:34 +0100

    refactor : create new struct to handle data

---
+++ StringCalculator/InputAndSeparators.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata
+{
+    public struct InputAndSeparators
+    {
+        public IEnumerable<char> separators;
+        public string input;
+    }
+}
